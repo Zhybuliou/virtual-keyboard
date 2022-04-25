c2c0403b0be75fac64356dab78590e9e751ada36
Vitali Zhybuliou <ya.vitalik-23@yandex.ru>
2022-04-25 21:09:41 +0300

    feat: add when type on keybord show on screen

--- virtual-keyboard/src/index.js
+++ virtual-keyboard/src/index.js
@@ -7,10 +7,31 @@ import keyboardShow from './layouts/keyboard/keyboard';
 import footerShow from './layouts/footer/footer';
 
 const BODY = document.getElementsByTagName('body')[0];
-
+const language = 'en';
 HeaderShow(BODY);
 textAreaShow(BODY);
-keyboardShow(BODY, keysB, 'enUp');
+keyboardShow(BODY, keysB, language);
 footerShow(BODY);
 
 focusTextArea();
+
+const KeyDown = [];
+
+document.addEventListener('keydown', (event) => {
+  if (event.code === 'ControlLeft' || event.code === 'AltLeft') {
+    event.preventDefault();
+  }
+  const someKeyDown = document.querySelector(`#${event.code}`);
+  someKeyDown.classList.add('key-Up');
+  if (!KeyDown.includes(event.code)) KeyDown.push(event.code);
+  if (event.code === 'Tab') {
+    event.preventDefault();
+    document.querySelector('#textarea').value += '    ';
+  }
+});
+document.addEventListener('keyup', (event) => {
+  const someKeyDown = document.querySelector(`#${event.code}`);
+  KeyDown.pop(event.code);
+  someKeyDown.classList.remove('key-Up');
+  focusTextArea();
+});
--- virtual-keyboard/src/layouts/keyboard/keyboard.js
+++ virtual-keyboard/src/layouts/keyboard/keyboard.js
@@ -7,35 +7,35 @@ const keyboardShow = (body, keys, lang) => {
     const key = document.createElement('div');
     if (el.en === 'Backspace') {
       key.classList = 'keys-backspace';
-      key.id = el.en;
+      key.id = el.code;
       key.innerText = el.en;
     } else if (el.en === 'Tab') {
       key.classList = 'keys-tab';
-      key.id = el.en;
+      key.id = el.code;
       key.innerText = el.en;
     } else if (el.en === 'Enter' || (el.en === 'Shift' && el.small === 'Shift')) {
       key.classList = 'keys-86';
-      key.id = el.en;
+      key.id = el.code;
       key.innerText = el.en;
     } else if (el.en === 'CapsLock' || el.en === 'Shift') {
       key.classList = 'keys-100';
-      key.id = el.en;
+      key.id = el.code;
       key.innerText = el.en;
     } else if (arrayKey.includes(el.en)) {
       key.classList = 'keys-40';
-      key.id = el.en;
+      key.id = el.code;
       key.innerText = el.en;
     } else if (el.en === 'Space') {
       key.classList = 'keys-space';
-      key.id = el.en;
+      key.id = el.code;
       key.innerText = el.en;
     } else if (el.en === 'Del') {
       key.classList = 'keys-del';
-      key.id = el.en;
+      key.id = el.code;
       key.innerText = el.en;
     } else {
       key.classList = 'keys-letter';
-      key.id = el.en;
+      key.id = el.code;
       if (lang === 'en') {
         key.innerText = el.en;
       } else if (lang === 'enUp') {
